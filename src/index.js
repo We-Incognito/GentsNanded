95d81be8e95ffd155135ebd3a54bf9588ac0cab8
Incognito <weincognito0@gmail.com>
2024-01-05 16:36:47 +0530

    Update Data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../src/Assets/Style/Style.scss"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
