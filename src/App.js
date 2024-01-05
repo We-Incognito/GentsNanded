cc643441685465a30a19480788418e2f3acd25f3
Incognito <weincognito0@gmail.com>
2024-01-05 16:24:50 +0530

    App.Js Modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Header from './Components/Header.jsx'
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      
     </div>
   );
 };
