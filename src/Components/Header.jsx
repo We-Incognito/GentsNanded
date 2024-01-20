b52753b4d9af31d5bea127bbed43d0431f0c74d3
Incognito <weincognito0@gmail.com>
2024-01-20 16:54:16 +0530

    Modify Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,41 +1,106 @@
 import React from "react";
-import { FaRegHeart, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaHeart, FaSearch, FaShoppingCart, FaUserAlt } from "react-icons/fa";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../Assets/Images/logo-small.jpg";
 import { NavLink } from "react-router-dom";
 
-
 const Header = () => {
   return (
     <div className="cx-header">
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/home">
             <img src={logo} alt="logo" className="img-logo" />
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav ms-2" />
           <Navbar.Collapse id="basic-navbar-nav ">
             <Nav className="nav-link">
-              <NavLink to="/home">Home</NavLink>
-              <NavLink to="/Mens">Mens</NavLink>
-              <NavLink to="/Kids">Kids</NavLink>
-              <NavLink to="/About">About</NavLink>
+              <NavLink
+                className="link-wrapper"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  padding: "10px",
+                  fontWeight: 500,
+                }}
+                to="/home"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className="link-wrapper"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  padding: "10px",
+                  fontWeight: 500,
+                }}
+                to="/Mens"
+              >
+                Mens
+              </NavLink>
+              <NavLink
+                className="link-wrapper"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  padding: "10px",
+                  fontWeight: 500,
+                }}
+                to="/Kids"
+              >
+                Kids
+              </NavLink>
+              <NavLink
+                className="link-wrapper"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  padding: "10px",
+                  fontWeight: 500,
+                }}
+                to="/About"
+              >
+                About
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
           <div className="right-header">
-            <button className="icon-btn">
+            <button
+              style={{
+                color: "white",
+              }}
+              className="icon-btn"
+            >
               <FaSearch />
             </button>
-            <button className="icon-btn">
-              <FaRegHeart />
+            <button
+              style={{
+                color: "white",
+              }}
+              className="icon-btn"
+            >
+              <FaHeart />
             </button>
-            <button className="icon-btn">
+            <button
+              style={{
+                color: "white",
+              }}
+              className="icon-btn"
+            >
               <FaShoppingCart />
             </button>
-            <button className="icon-btn">Login</button>
+            <button
+              style={{
+                color: "white",
+              }}
+              className="icon-btn"
+            >
+              <FaUserAlt />
+            </button>
           </div>
         </Container>
       </Navbar>
